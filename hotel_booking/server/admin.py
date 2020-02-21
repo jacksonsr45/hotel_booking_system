d5c2f0f938cfca563a2aed82bf55251155bdfc0c
Jackson <jacksonsr45@gmail.com>
2020-02-21 15:53:21 -0300

    add in .gitignore settings.py

--- hotel_booking/server/admin.py
+++ hotel_booking/server/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+admin.site.register(room)
+admin.site.register(countries)
+admin.site.register(booking)
+admin.site.register(categories)
+admin.site.register(customer)
+admin.site.register(amount_booking)
